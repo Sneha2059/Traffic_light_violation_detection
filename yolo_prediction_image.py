e3b863990e208570ba64cac6074714ab62e55a4a
Sneha2059 <snehapoudel00896@gmail.com>
2024-09-14 23:33:43 +0545

    added ROI

--- yolo_prediction_image.py
+++ yolo_prediction_image.py
@@ -17,21 +17,23 @@ for result in results:
     for i, (box, score, cls) in enumerate(zip(boxes, confs, classes)):
         x_min, y_min, x_max, y_max =map(int,box)
         class_id = int(cls)
-        class_name = model.names[class_id]  # Human-readable class name
+        class_name = model.names[class_id]  # class name
 
         #print(f"Object {i}:")
        # print(f"  Coordinates: ({x_min:.2f}, {y_min:.2f}), ({x_max:.2f}, {y_max:.2f})")
        # print(f"  Confidence: {score:.2f}")
         #print(f"  Class ID: {class_id}, Class Name: {class_name}")
         text=f"{class_name}:{score:.2f}%"
+        
 
         cv2.rectangle(img,(x_min,y_min),(x_max,y_max),(0,255,0),2)
         #cv2.rectangle(img,(x_min,y_min-30),(x_max,y_min),(255,255,255),-1)
 
         cv2.putText(img,text,(x_min,y_min-10),cv2.FONT_HERSHEY_PLAIN,0.7,(0,0,0),1)
+        
 
-cv2.imwrite('./2.Predictions/traffic_streets_with_boxes.jpeg', img)  # Save result
-cv2.imshow('Image with Bounding Boxes', img)  # Display result
-cv2.waitKey(0)  # Wait for a key press to close the window
-cv2.destroyAllWindows()  # Close all OpenCV windows
+    cv2.imwrite('./2.Predictions/traffic_streets_with_boxes.jpeg', img)  # Save result
+    cv2.imshow('Image with Bounding Boxes', img)  # Display result
+    cv2.waitKey(0)  # Wait for a key press to close the window
+    cv2.destroyAllWindows()  # Close all OpenCV windows
 
--- yolo_prediction_video.py
+++ yolo_prediction_video.py
@@ -13,9 +13,10 @@ while cap.isOpened():
         print("number of frames have finished.")
         break
     else:
-        frame = cv2.resize(frame, (1200, 720))
+        #frame = cv2.resize(frame, (1200, 720))
         # Get predictions
-        
+        cv2.namedWindow('./2.Predictions/traffic_video.mp4', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('./2.Predictions/traffic_video.mp4', 1920, 1080)
         results = model(frame)
         for result in results:
             boxes = result.boxes.xyxy
@@ -25,6 +26,10 @@ while cap.isOpened():
                 x_min, y_min, x_max, y_max =map(int,box)
                 class_id = int(cls)
                 class_name = model.names[class_id]  # Human-readable class name
+                if x_min==259 and y_min==560:
+                    print("rules violated")
+                    cv2.putText(frame,'Traffic violated',(20,30),cv2.FONT_HERSHEY_PLAIN,0.7,(0,0,0),1)
+
 
                 print(f"Object {i}:")
                 print(f"  Coordinates: ({x_min:.2f}, {y_min:.2f}), ({x_max:.2f}, {y_max:.2f})")
@@ -37,8 +42,8 @@ while cap.isOpened():
 
                 cv2.putText(frame,text,(x_min,y_min-10),cv2.FONT_HERSHEY_PLAIN,0.7,(0,0,0),1)
 
-        
-        cv2.imshow('Image with Bounding Boxes', frame)  # Display result
+        cv2.rectangle(frame,(259,560),(1591,688),(0,0,255),3)
+        cv2.imshow('./2.Predictions/traffic_video.mp4', frame)  # Display result
         if cv2.waitKey(1) == 27:
             break
 cap.release()
